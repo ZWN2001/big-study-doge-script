33545cbacc2681abbc26cd653022426e303f46f7
Karl Eric <2134356214@qq.com>
2023-01-03 19:54:28 +0800

    feat:修正openid参数为openid列表以支持多人情况

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import requests
 import re
 
-openid = '----'
+openids = ['----']
 printHistory = False
 proxies = {"https": 'https://127.0.0.1:7890'}
 
 
-def getHistory(current_version):
+def getHistory(current_version, openid):
     url = "http://qndxx.youth54.cn/SmartLA/dxx.w?method=queryPersonStudyRecord"
     headers = {
         "Origin": "http://qndxx.youth54.cn",
         "Cookie": "JSESSIONID=277C3E9C8D096A7A5025C8395A58A25D",
         "Accept": "*/*",
         "X-Requested-With": "XMLHttpRequest",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4365 MMWEBSDK/20221012 Mobile Safari/537.36 MMWEBID/6920 MicroMessenger/8.0.30.2260(0x28001E55) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 12; ELS-AN00 Build/HUAWEIELS-AN00; wv) AppleWebKit/537.36 (KHTML, "
+                      "like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4365 MMWEBSDK/20221012 Mobile Safari/537.36 "
+                      "MMWEBID/6920 MicroMessenger/8.0.30.2260(0x28001E55) WeChat/arm64 Weixin NetType/WIFI "
+                      "Language/zh_CN ABI/arm64",
         "Connection": "keep-alive",
         "Referer": "http://qndxx.youth54.cn/SmartLA/dxx.w?method=enterIndexPage&fxopenid=&fxversion=",
         "Host": "qndxx.youth54.cn",
@@ -59,7 +62,8 @@ def getNewestVersionInfo():
         "Cookie": "JSESSIONID=A0B36BB8AC5782093C96170306EECFD4",
         "Accept": "*/*",
         "X-Requested-With": "XMLHttpRequest",
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.16(0x18001031) NetType/WIFI Language/zh_CN",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, "
+                      "like Gecko) Mobile/15E148 MicroMessenger/8.0.16(0x18001031) NetType/WIFI Language/zh_CN",
         "Connection": "keep-alive",
         "Referer": "http://qndxx.youth54.cn/SmartLA/dxx.w?method=enterIndexPage&fxopenid=&fxversion=",
         "Host": "qndxx.youth54.cn",
@@ -102,21 +106,23 @@ def passInfo():
     }
 
     info = getNewestVersionInfo()
-    done = getHistory(info)
+    openidsWithoutRepate = list(set(openids))
+    for openid in openidsWithoutRepate:
+        done = getHistory(info, openid)
 
-    if not done:
-        version = info
-        # TODO:抓包，"http://qndxx.youth54.cn/SmartLA/dxxjfgl.w?method=getNewestVersionInfo"等url内都可以找到openID字段
-        data = f"openid={openid}&version={version}"
-        try:
-            response = requests.post(url, data=data, headers=headers, verify=False, timeout=5, proxies=proxies)
-            if response.status_code == 200 and response.json()["errcode"] == "0":
-                print("[*] Success ")
-        except:
-            print("error")
-            pass
-    else:
-        print("you have done this")
+        if not done:
+            version = info
+            # TODO:抓包，"http://qndxx.youth54.cn/SmartLA/dxxjfgl.w?method=getNewestVersionInfo"等url内都可以找到openID字段
+            data = f"openid={openid}&version={version}"
+            try:
+                response = requests.post(url, data=data, headers=headers, verify=False, timeout=5, proxies=proxies)
+                if response.status_code == 200 and response.json()["errcode"] == "0":
+                    print(f"{openid}: Success ")
+            except:
+                print(f"{openid}:error")
+                pass
+        else:
+            print(f"{openid} have done this term")
 
 
 if __name__ == "__main__":
